74dbd75acf02d1d3b6a020261a583a52cb6f9059
Tejasvita <ttejasvita@gmail.com>
2026-05-17 23:50:55 +0530

    Initial Spring Boot app

---
+++ src/main/java/com.devops.dockercidemo/DockerCiDemoApplication.java
@@ -0,0 +1,13 @@
+package docker_ci_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerCiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/docker_ci_demo/DockerCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package docker_ci_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerCiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
